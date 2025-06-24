300e47eb87fc363dbdc0a3bb3baee73dcd77f727
Asit-14 <asitshakya789@gmail.com>
2025-06-24 19:58:30 +0530

    anonymous function expression example in JavaScript

---
+++ JavaScript/functionexpression.js
@@ -0,0 +1,8 @@
+// name function expression
+// anonynmous function expression
+
+let x = function() {
+	console.log("Hello from an anonymous function expression!");
+}	
+
+x(); // Call the function
