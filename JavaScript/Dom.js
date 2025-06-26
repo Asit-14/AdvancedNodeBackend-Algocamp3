33ce14431be1c503136bb4be413c223e69c112a5
Asit-14 <asitshakya789@gmail.com>
2025-06-26 16:39:48 +0530

    add DOM manipulation examples in JavaScript

---
+++ JavaScript/Dom.js
@@ -0,0 +1,14 @@
+let var1 = document.getElementById("one");
+console.log(var1.innerText);
+
+let var2 = document.getElementsByClassName("tech");
+console.log(var2[2].innerText);
+
+var2[1].innerText = "JavaScript";
+
+let var3 = document.getElementById("h4");
+console.log(var3[1].innerHTML);
+
+var3[2].innerHTML = "Hellow  Asit Kumar";
+
+var3[2].style.color = "red";		
