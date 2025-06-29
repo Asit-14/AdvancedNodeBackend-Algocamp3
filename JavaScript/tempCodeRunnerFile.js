c9491d71c73388e9f9d5a83555a15b4c6cdffbe3
Asit-14 <asitshakya789@gmail.com>
2025-06-29 22:42:58 +0530

    js  temp  file

---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+// const heros = ["Virat", "Tiger", "salman", "varun"];
+// console.log(heros);
+
+// const herowitman = heros.filter((h) => {
+// 	return h.endsWith("man")
+// })
+// console.log(herowithman)
+
