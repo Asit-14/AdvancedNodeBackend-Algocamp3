5ffdf5d7035079d6fad315408ccc28006df0c650
Asit-14 <asitshakya789@gmail.com>
2025-07-01 14:31:44 +0530

    add product class with properties and display method

---
+++ JavaScript/classintro.js
@@ -0,0 +1,18 @@
+class product {
+
+	// Properties
+	name;
+	price;
+	rating;
+
+	//functions
+
+	display() {
+		console.log("This is a product class");
+	}
+}
+
+const p = new product();
+console.log(p);
+// p.name = "Asit Kumar ";
+p.display(); 
