a01e26e602005703221df7df784ff253ec01e675
Asit-14 <asitshakya789@gmail.com>
2025-07-07 16:10:39 +0530

    undersantd the trim and length

--- JavaScript/prototype.js
+++ JavaScript/prototype.js
@@ -1,50 +1,57 @@
 let myhero = ["thor", "ironman", "hulk"];
 let dcheros = ["batman", "superman", "spiderman"];
 let heropower = {
-    thor: "lightning",
-    ironman: "technology",
-    hulk: "strength",
-    batman: "intelligence",
+  thor: "lightning",
+  ironman: "technology",
+  hulk: "strength",
+  batman: "intelligence",
 
-    getthorpower: function () {
-        console.log("Thor's power is " + this.thor);
-    }
-}
+  getthorpower: function () {
+    console.log("Thor's power is " + this.thor);
+  },
+};
 Object.prototype.Asit = function () {
-    console.log("Asit is a good")
-}
+  console.log("Asit is a good");
+};
 
 console.log(myhero.Asit()); // []
 console.log(heropower.Asit()); // []
 Array.prototype.heyasit = function () {
-    console.log("Hey Asit, I am an array method now!");
-
-}
-
+  console.log("Hey Asit, I am an array method now!");
+};
 
 console.log(myhero.heyasit()); // Hey Asit, I am an array method now!
 // console.log(heropower.heyasit()); // Hey Asit, I am an array method
 
-
 // inheritance
 const User = {
-    name: "Asit",
-    age: 25,
-    email :"Asitshakya789@gmail.com"
-}
+  name: "Asit",
+  age: 25,
+  email: "Asitshakya789@gmail.com",
+};
 
 const teacher = {
-    makevideo : true
-}
+  makevideo: true,
+};
 
 const teachingsupport = {
-    isAvailable: true
-}
+  isAvailable: true,
+};
 
 const taassignament = {
-    makeassignment: "Js  Assignemet",
-    fulltime: true,
-    __proto__: teachingsupport
-}
+  makeassignment: "Js  Assignemet",
+  fulltime: true,
+  __proto__: teachingsupport,
+};
+Object.setPrototypeOf(teachingsupport, teacher);
 
-taassignament.isAvailable // tru
+console.log(taassignament.makeassignment); // Js  Assignemet
+
+String.prototype.truelength = function () {
+  console.log(
+    `The true length of the string is ${this.trim().length} characters.`
+  );
+};
+
+"Hello World!  ".truelength(); // The true length of the string is 12 characters.
+"   Asit Shakya      ".truelength(); // The true length of the string is 14 characters.
